3851bec02cb5d1cde5169e893167a9515da976a9
Nikolajr93 <nikolajr93@gmail.com>
2022-11-16 23:07:50 +0100

    Commiting MyClassTwo for the first time.

---
+++ src/main/java/rs/raf/MyClassTwo.java
@@ -0,0 +1,14 @@
+package rs.raf;
+
+public class MyClassTwo {
+
+    private Object MyAttributeTwo;
+
+    public Object getMyAttributeTwo() {
+        return MyAttributeTwo;
+    }
+
+    public void setMyAttributeTwo(Object myAttributeTwo) {
+        MyAttributeTwo = myAttributeTwo;
+    }
+}
