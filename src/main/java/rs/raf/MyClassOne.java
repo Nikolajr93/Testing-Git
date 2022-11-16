8b32877f0d6508be4f6db7e67e3e27c138b88cb7
Nikolajr93 <nikolajr93@gmail.com>
2022-11-16 22:58:54 +0100

    MyClassOne first commit

---
+++ src/main/java/rs/raf/MyClassOne.java
@@ -0,0 +1,16 @@
+package rs.raf;
+
+public class MyClassOne {
+
+    private Object MyAttributeOne;
+
+
+
+    public Object getMyAttributeOne() {
+        return MyAttributeOne;
+    }
+
+    public void setMyAttributeOne(Object myAttributeOne) {
+        MyAttributeOne = myAttributeOne;
+    }
+}
