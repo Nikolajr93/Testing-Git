04b0adf86747df786bf9e7505273dc94dc1db10e
Nikolajr93 <nikolajr93@gmail.com>
2022-11-16 23:17:26 +0100

    MyClassFour commit, creating a second branch.

---
+++ src/main/java/rs/raf/devTwoPack/MyClassFour.java
@@ -0,0 +1,14 @@
+package rs.raf.devTwoPack;
+
+public class MyClassFour {
+
+    private Object MyAttributeFour;
+
+    public Object getMyAttributeFour() {
+        return MyAttributeFour;
+    }
+
+    public void setMyAttributeFour(Object myAttributeFour) {
+        MyAttributeFour = myAttributeFour;
+    }
+}
